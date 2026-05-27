8f12f2a918174b4c538be23e89106a2c6e3445ca
Mzzj114 <1174931367@qq.com>
2026-05-27 08:39:19 -0400

    refactor: remove verbose passwd validator re user feedback

--- know_more/settings/base.py
+++ know_more/settings/base.py
@@ -75,9 +75,6 @@ WSGI_APPLICATION = 'know_more.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
